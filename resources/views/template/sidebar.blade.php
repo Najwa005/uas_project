014496855806f9df7dcea5dbc5b1676830fca784
Najwa005 <najwafitriyani05@gmail.com>
2024-07-23 21:46:39 +0800

    UAS CRUD SI Akademik

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -1,11 +1,23 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="welcome" class="brand-link">
-        <img src="{{ asset ('dist/img/logopoliban.png') }}" alt="Logo" class="brand-image img-circle elevation-3"
+        <img src="{{ asset('dist/img/logopoliban.png') }}" alt="Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
         <h3><span class="brand-text font-weight-light">POLIBAN</span></h3>
     </a>
-    
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="{{ asset('images/' . $data['foto']) }}" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">{{ $data['nama'] }}</a>
+            </div>
+        </div>
+
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
