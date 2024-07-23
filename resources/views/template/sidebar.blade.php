40fc5623f796f16393a2a59fe537655af6811af0
Najwa005 <najwafitriyani05@gmail.com>
2024-07-23 21:26:52 +0800

    UAS CRUD SI Akademik

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -5,19 +5,7 @@
             style="opacity: .8">
         <h3><span class="brand-text font-weight-light">POLIBAN</span></h3>
     </a>
-
-    <!-- Sidebar -->
-    <div class="sidebar">
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src=" class="img-circle elevation-2" alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block"></a>
-            </div>
-        </div>
-
+    
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
